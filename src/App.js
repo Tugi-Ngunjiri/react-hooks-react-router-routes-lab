e86d5a6a19f8bfe5f8a30e1e6bee3ecd4d158adf
tugi <blueqwinn@gmail.com>
2023-01-18 14:12:54 +0300

    addapps

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.component{
+    render(){
+return(
+<h1>Home</h1>
+
+)
+
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import {Route,Link,BrowserRouter  as Router } from "react-router-dom";
 import App from "./components/App";
+import './index.css';
+import About from "./about";
+import Contact from "./contact";
 
+const routing =(
+<Router>
+<div>
+  <h1>React Router Example</h1>
+<route path="/" component={App}/>
+<route path="/about" component={Contact}/>
+<route path="/contact" component={Contact}/>
+</div>
+</Router>
+)
 ReactDOM.render(
   <Router>
+
     <App />
   </Router>,
   document.getElementById("root")
 );
+
+ReactDOM.render(routing,document.getElementById('root'))
