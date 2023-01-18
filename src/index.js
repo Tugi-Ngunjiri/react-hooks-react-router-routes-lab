87dcc1695918e2068739cc7dd492086aa65e571b
tugi <blueqwinn@gmail.com>
2023-01-18 14:45:30 +0300

    adddeliverables

--- src/index.js
+++ src/index.js
@@ -6,6 +6,60 @@ import './index.css';
 import About from "./about";
 import Contact from "./contact";
 
+const Routing =(
+<Router>
+<div>
+<h1>React Router</h1>
+<ul>
+<li>
+<link to="/">Home</link>
+</li>
+<li>
+<link to="/">About</link>
+</li>
+
+<li>
+<link to="/">Contact</link>
+</li>
+</ul>
+<Route exact path="/" component ={App}/>
+<Route path="/about" component ={About}/>
+<Route path="/contact" component ={Contact}/>
+
+</div>
+</Router>
+
+
+
+)
+
+const RouTing = (  
+  <Router>  
+    <div>  
+      <h1>React Router Example</h1>  
+      <ul>  
+        <li>  
+          <NavLink to="/" exact activeStyle={  
+             {color:'red'}  
+          }>Home</NavLink>  
+        </li>  
+        <li>  
+          <NavLink to="/about" exact activeStyle={  
+             {color:'green'}  
+          }>About</NavLink>  
+        </li>  
+        <li>  
+          <NavLink to="/contact" exact activeStyle={  
+             {color:'magenta'}  
+          }>Contact</NavLink>  
+        </li>  
+      </ul>  
+      <Route exact path="/" component={App} />  
+      <Route path="/about" component={About} />  
+      <Route path="/contact" component={Contact} />  
+    </div>  
+  </Router>  
+)  
 const routing =(
 <Router>
 <div>
@@ -23,5 +77,7 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
+
 
 ReactDOM.render(routing,document.getElementById('root'))
